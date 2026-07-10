cb92168d7876fccf1d6c9433169e79edbad692ca
shodrenars <renarsups2@gmail.com>
2026-07-10 14:24:27 +0300

    05

--- CPP/05 loops.cpp
+++ CPP/05 loops.cpp
@@ -7,13 +7,14 @@ int main() {
 // Initialization(declare and set a starting value for a loop counter variable)
 // Condition (if returns true, the loop will continue to run)
 // Update (increment or decrement the loop counter variable)
-// if checks before the loop runs
+// Loops certain amount of times
         a ++;
         cout << "for: " << a << endl;
     }
 
     while(b < 5) {
 // Condition (if returns true, the loop will continue to run)
+// Loops infinitely until the condition is false
         b++;
         cout << "while: " << b << endl;
     }
@@ -24,5 +25,11 @@ int main() {
     } while(c < 5);
 // Condition (if returns true, the loop will continue to run)
 // do-while checks after the loop runs
+// Loops at least once, then checks the condition 
+
+// Loops can be interrupted with these statements:
+// break - exits the loop entirely
+// continue - skips the rest of the loop and goes to the next iteration
+// Loops can be nested inside of each other
     return 0;
 }
